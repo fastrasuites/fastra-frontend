830a7191c83cebde3ea9306e5c63d927a06b899f
oladokunlt <lukmanoladokun86@gmail.com>
2024-08-26 20:34:26 +0100

    Updated: Responsiveness to dashboard popup

--- src/components/StepModal.jsx
+++ src/components/StepModal.jsx
@@ -24,77 +24,16 @@ const StepModal = ({ open, onClose, step, onNextStep }) => {
           Skip
         </button>
         <Box sx={{ position: "relative", height: "100%" }}>
-          <Box
-            sx={{
-              position: "absolute",
-              left: "32px",
-              top: "0",
-              buttom: "10px",
-              width: "4px",
-              height: "calc(100% - 50px)",
-              bgcolor: "#3B7CED",
-            }}
-          />
+          <div className="vertical-line"></div>
 
-          <Box
-            sx={{
-              position: "absolute",
-              left: "-15px",
-              top: "100px",
-              width: "60px",
-              height: "60px",
-              bgcolor: "#E2E6E9",
-              color: "#3B7CED",
-              fontWeight: "900",
-              lineHeight: "58.22px",
-              fontSize: "48px",
-              borderRadius: "50%",
-              display: "flex",
-              justifyContent: "center",
-              alignItems: "center",
-              marginTop: "10px",
-              marginLeft: "10px",
-              border: "10px solid white",
-            }}
-          >
-            {step}
-          </Box>
-          <Box
-            sx={{
-              position: "absolute",
-              left: "-15px",
-              bottom: "10px",
-              width: "60px",
-              height: "60px",
-              bgcolor: "#E2E6E9",
-              color: "#3B7CED",
-              fontWeight: "900",
-              lineHeight: "58.22px",
-              fontSize: "48px",
-              borderRadius: "50%",
-              display: "flex",
-              justifyContent: "center",
-              alignItems: "center",
-              marginLeft: "10px",
-              border: "10px solid white",
-            }}
-          >
+          <div className="numbered-circle numbered-circle-one">{step}</div>
+
+          <div className="numbered-circle numbered-circle-two">
             {step + 1 <= 3 ? step + 1 : ""}
-          </Box>
+          </div>
           {/* Step 1  -------------------------------- */}
-          <div style={{ marginLeft: "120px", paddingTop: "70px" }}>
-            <Typography
-              sx={{
-                color: "#3B7CED",
-                marginBottom: "14px",
-                textAlign: "left",
-                fontSize: "20px",
-                fontWeight: "700",
-                lineHeight: "24.26px",
-              }}
-            >
-              3 SIMPLE STEPS
-            </Typography>
+          <div className="modal-main-content">
+            <p className="para-headline">3 SIMPLE STEPS</p>
             <p className="heading-text">
               {step === 1
                 ? "Step 1: Company Setting"
@@ -104,17 +43,7 @@ const StepModal = ({ open, onClose, step, onNextStep }) => {
                 ? "step 3: Project Creation"
                 : "Registration Successful"}
             </p>
-            <Typography
-              sx={{
-                fontFamily: "Product Sans",
-                fontSize: "14px",
-                fontWeight: "400",
-                lineHeight: "16.98px",
-                textAlign: "left",
-                color: "#7A8A98",
-                marginBottom: "40px",
-              }}
-            >
+            <p className="para-description">
               {step === 1
                 ? "A company set-up will ease your journey using this platform."
                 : step === 2
@@ -122,30 +51,8 @@ const StepModal = ({ open, onClose, step, onNextStep }) => {
                 : step === 3
                 ? "Now at the last step, Enjoy seamless Experience Here."
                 : ""}
-            </Typography>
-            <Button
-              onClick={handleSetUpCompany}
-              sx={{
-                display: "flex",
-                flexDirection: "row",
-                justifyContent: "start",
-                alignItems: "center",
-                padding: "16px 20px",
-                background: "#3B7CED",
-                boxShadow: "0px 32px 24px rgba(26, 26, 26, 0.08)",
-                borderRadius: "4px",
-                fontStyle: "normal",
-                fontWeight: "400",
-                fontSize: "18px",
-                lineHeight: "24px",
-                textAlign: "center",
-                color: "#FFFFFF",
-                marginBottom: "72px",
-                "&:hover": {
-                  background: "#3367CC",
-                },
-              }}
-            >
+            </p>
+            <button onClick={handleSetUpCompany} className="btn-goto-steps">
               {step === 1
                 ? "Set up your Company!"
                 : step === 2
@@ -153,56 +60,27 @@ const StepModal = ({ open, onClose, step, onNextStep }) => {
                 : step === 3
                 ? "Start Project Creation!"
                 : "You are done"}
-            </Button>
+            </button>
 
             {/* End Step 1 ------------------------------ */}
 
             {/* Step 2 ---------------------------------- */}
 
-            <Typography
-              sx={{
-                color: "#3B7CED",
-                marginBottom: "16px",
-                textAlign: "left",
-                fontSize: "20px",
-                fontWeight: "700",
-                lineHeight: "24.26px",
-              }}
-            >
-              3 SIMPLE STEPS
-            </Typography>
-            <Typography
-              sx={{
-                fontWeight: "900",
-                fontSize: "40px",
-                textAlign: "left",
-                color: "#1A1A1A",
-                marginBottom: "4px",
-              }}
-            >
+            <p className="para-headline">3 SIMPLE STEPS</p>
+            <p className="heading-text">
               {step === 1
                 ? "Step 2: User Creation"
                 : step === 2
                 ? "step 3: Project Creation"
                 : "Registration Successful"}
-            </Typography>
-            <Typography
-              sx={{
-                fontFamily: "Product Sans",
-                fontSize: "14px",
-                fontWeight: "400",
-                lineHeight: "16.98px",
-                textAlign: "left",
-                color: "#7A8A98",
-                marginBottom: "40px",
-              }}
-            >
+            </p>
+            <p className="para-description">
               {step === 1
                 ? "Awesome! Just a few steps to start having an Amazing Fastra Experience"
                 : step === 2
                 ? "Now at the last step, Enjoy seamless Experience Here."
                 : ""}
-            </Typography>
+            </p>
           </div>
           {/* End step 2 ------------------------------ */}
         </Box>
