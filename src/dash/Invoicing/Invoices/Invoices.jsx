6baf2f3c211fe40a5c97bfbf69700110765d9d5d
oladokunlt <lukmanoladokun86@gmail.com>
2025-09-23 13:27:20 +0100

    feat: Add Invoicing feature with routes, components, and header; enhance sidebar with invoicing link

---
+++ src/dash/Invoicing/Invoices/Invoices.jsx
@@ -0,0 +1,12 @@
+import { Box, Typography } from "@mui/material";
+import React from "react";
+
+const Invoices = () => {
+  return (
+    <Box p={3}>
+      <Typography>Invoices</Typography>
+    </Box>
+  );
+};
+
+export default Invoices;
---
+++ src/dash/Invoicing/InvoicingHeader.jsx
@@ -0,0 +1,51 @@
+import React from "react";
+import DashboardHeader from "../DashboardHeader/DashboardHeader";
+import { useTenant } from "../../context/TenantContext";
+
+const InvoicingHeader = () => {
+  const { tenantData } = useTenant();
+  const tenant_schema_name = tenantData?.tenant_schema_name;
+
+  const menuItems = [];
+
+  const subItems = [];
+
+  subItems.push({
+    label: "Paid Invoices",
+    link: `/${tenant_schema_name}/invoicing/invoices`,
+  });
+
+  subItems.push({
+    label: "Partially Paid Invoices",
+    link: `/${tenant_schema_name}/invoicing/invoices/partially-paid-invoices`,
+  });
+
+  subItems.push({
+    label: "Unpaid Invoices",
+    link: `/${tenant_schema_name}/invoicing/invoices/unpaid-invoices`,
+  });
+
+  if (subItems.length > 0) {
+    menuItems.push({
+      label: "Invoices",
+      link: `/${tenant_schema_name}/invoicing/invoices`,
+      subItems,
+    });
+  }
+
+  // Payments -------------------------
+  menuItems.push({
+    label: "Payments",
+    link: `/${tenant_schema_name}/invoicing/payments`,
+  });
+
+  // Add more menu items and submenu items below
+
+  return (
+    <div>
+      <DashboardHeader title="Invoices" menuItems={menuItems} />
+    </div>
+  );
+};
+
+export default InvoicingHeader;
